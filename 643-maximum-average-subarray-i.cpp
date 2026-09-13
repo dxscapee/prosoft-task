3d8bf9cf823c932376ac33725e825df1a5abb069
Gleb <glebyazo@gmail.com>
2026-09-13 16:48:24 +0500

    medium

--- 643-maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i.cpp
@@ -8,26 +8,26 @@ class Solution {
 public:
     double findMaxAverage(const vector<int>& nums, int k) {
         
-        //собираем сумму первых k элементов
-        //записываем результат именно в max_sum, чтобы в дальнешем корректно сравнивать
+        //Собираем сумму первых k элементов.
+        //Записываем результат именно в max_sum, чтобы в дальнешем корректно сравнивать
         //(по условию элементы могут быть <0 из-за чего нельзя задать max_sum = 0)
         double max_sum = 0;
         for (int i = 0; i < k; ++i){
             max_sum += nums[i];
         }
 
-        //при помощи скользящего окна ищем самую большую сумму k элементов
+        //При помощи скользящего окна ищем самую большую сумму k элементов
         double curr_sum = max_sum;
         for (int i = k;i < nums.size(); ++i){
             curr_sum-=nums[i-k];
             curr_sum+=nums[i];
             max_sum = std::max(curr_sum,max_sum);
         }
-        //возвращаем среднее значение (max_sum делённое на k)
+        //Возвращаем среднее значение (max_sum делённое на k)
         return max_sum / k;
     }
 };
 
-//важный момент: для max_sum и curr_sum используем именно double, а не int или long long, потому что:
+//Важный момент: для max_sum и curr_sum используем именно double, а не int или long long, потому что:
 //при типе данных int мы рискуем переполнить переменные (пример: max_sum += INT_MAX, max_sum += INT_MAX)
 //double, а не long, чтобы проще вернуть вещественную max_average без использования static_cast<double> и тп
